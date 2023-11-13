887206e34ce9d29047b5cc49f29debe7e0cee011
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2023-11-13 15:56:12 +0530

    updates on database

--- script.js
+++ script.js
@@ -20,35 +20,43 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-    const openpopup1 = document.getElementById("open-popup1")
-    const openpopup2 = document.getElementById("open-popup2")
-    const openpopup3 = document.getElementById("open-popup3")
-    const openpopup4 = document.getElementById("open-popup4")
-    const closepopup = document.getElementById("close-popup")
-    
-    const q1 = document.getElementById('q1')
-    const q2 = document.getElementById('q2')
-    const q3 = document.getElementById('q3')
-    const q4 = document.getElementById('q4')
-    openpopup1.addEventListener('click',function(){
-        q1.style.display = 'block';
-    })
-    openpopup2.addEventListener('click',function(){
-        q2.style.display = 'block';
-    })
-    openpopup3.addEventListener('click',function(){
-        q3.style.display = 'block';
-    })
-    openpopup4.addEventListener('click',function(){
-        q4.style.display = 'block';
-    })
-
-    closepopup.addEventListener('click', function () {
-        // console.log("hello")
-        q1.style.display = 'none';
-        q2.style.display = 'none';
-        q3.style.display = 'none';
-        q4.style.display = 'none';
-    })
 });
 
+const openpopup1 = document.getElementById("open-popup1")
+const openpopup2 = document.getElementById("open-popup2")
+const openpopup3 = document.getElementById("open-popup3")
+const openpopup4 = document.getElementById("open-popup4")
+const closepopup1 = document.getElementById("close-popup1")
+const closepopup2 = document.getElementById("close-popup2")
+const closepopup3 = document.getElementById("close-popup3")
+const closepopup4 = document.getElementById("close-popup4")
+
+const q1 = document.getElementById('q1')
+const q2 = document.getElementById('q2')
+const q3 = document.getElementById('q3')
+const q4 = document.getElementById('q4')
+openpopup1.addEventListener('click',function(){
+    q1.style.display = 'block';
+})
+openpopup2.addEventListener('click',function(){
+    q2.style.display = 'block';
+})
+openpopup3.addEventListener('click',function(){
+    q3.style.display = 'block';
+})
+openpopup4.addEventListener('click',function(){
+    q4.style.display = 'block';
+})
+
+closepopup1.addEventListener('click', function () {
+    q1.style.display = 'none';
+})
+closepopup2.addEventListener('click', function () {
+    q2.style.display = 'none';
+})
+closepopup3.addEventListener('click', function () {
+    q3.style.display = 'none';
+})
+closepopup4.addEventListener('click', function () {
+    q4.style.display = 'none';
+})
